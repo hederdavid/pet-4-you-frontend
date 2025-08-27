0f814733026b1d082607056f93ae0ea43ab06f14
Your Name <hedermd6@gmail.com>
2025-08-27 16:04:04 -0300

    feat: firebase configs

--- src/boot/pinia.ts
+++ src/boot/pinia.ts
@@ -1,7 +1,22 @@
-import { boot } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
+// src/boot/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { boot } from 'quasar/wrappers';
 
-export default boot(({ app }) => {
-  const pinia = createPinia()
-  app.use(pinia)
-})
+// Cole a configuração do seu projeto Firebase aqui
+const firebaseConfig = {
+  apiKey: "AIzaSyAicqtf2InjWCQ0-w3lI-fAKP2cdxsVDDk",
+  authDomain: "backend-pet4you.firebaseapp.com",
+  projectId: "backend-pet4you",
+  storageBucket: "backend-pet4you.firebasestorage.app",
+  messagingSenderId: "955360629747",
+  appId: "1:955360629747:web:7a42f29cabde7fa95db886",
+  measurementId: "G-1KBDPDKKYM"
+};
+
+// Inicializa o Firebase
+initializeApp(firebaseConfig);
+
+export default boot(() => {
+  // Este arquivo de boot garante que o Firebase seja inicializado
+  // antes que qualquer outra parte do seu app tente usá-lo.
+});
